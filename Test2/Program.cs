97568de27a141cb65278e9c21032b0af69229905
Jakir <zakirsust2017@gmail.com>
2026-01-23 23:51:06 +0600

    a new project tested with nuget library

---
+++ Test2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using CustomHttp;
+using CustomServer;
+using CustomServerHttp;
+using CustomServerMvc;
+
+
+var builder = new ApplicationBuilder();
+// builder.Use(async (context, next) =>
+// {
+//     Console.WriteLine("First middleware");
+//     await next(context);
+// });
+//
+// builder.Use(async (context, next) =>
+// {
+//     Console.WriteLine("Second middleware");
+//     await next(context);
+// });
+
+
+var router = new TrieRouter();
+router.MapControllers(Assembly.GetExecutingAssembly());
+builder.Use(async (context, next) =>
+{
+    await router.UseRouting()(context);
+    await next(context);
+});
+
+var pipeline = builder.Build();
+var server = new CustomServer.CustomServer(pipeline);
+
+server.Start();
---
+++ Test2/UserDto.cs
@@ -0,0 +1,8 @@
+namespace Test;
+
+public class UserDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public double Age { get; set; }
+}
